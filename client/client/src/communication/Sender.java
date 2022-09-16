db27ccb3e53bc5c3172fb68593ae7bcc92f24ff5
SearcherX <93086610+SearcherX@users.noreply.github.com>
2022-09-16 11:29:38 +0300

    client v1.0

---
+++ client/client/src/communication/Sender.java
@@ -0,0 +1,34 @@
+package communication;
+
+import java.io.*;
+import java.net.Socket;
+
+// класс для отправки данных через сокет
+public class Sender {
+    private PrintWriter out = null; // поток для отправки данных
+
+    // конструктор
+    public Sender(Socket socket) throws IOException {
+        out = new PrintWriter(
+                new BufferedWriter(
+                        new OutputStreamWriter(socket.getOutputStream())
+                ),
+                true
+        );
+    }
+
+    // закрытие
+    public void close() {
+        if (out != null) {
+            out.close();
+            out = null;
+        }
+    }
+
+    // отправка сообщения
+    public void sendMsg(String msg) {
+        if (out != null) {
+            out.println(msg);
+        }
+    }
+}
